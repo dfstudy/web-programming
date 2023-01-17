0fa3dc713a63bfd7d0ef36646ff753847025c85b
dfstudy <113107711+dfstudy@users.noreply.github.com>
2023-01-17 13:50:32 +0300

    Исправления

---
+++ 4/scripts/index.js
@@ -0,0 +1,17 @@
+
+// 
+let items = document.querySelectorAll('.item');
+let btn_rbs = document.querySelectorAll(".btn-rb");
+
+for(let btn of btn_rbs)
+{
+    btn.addEventListener("click", function()
+    {
+        for(let item of items)
+        {
+            if(item.dataset.categories.indexOf(btn.dataset.categories)<0) 
+            item.style.display = "none";
+            else item.style.display = "block";
+        }
+    });
+}
